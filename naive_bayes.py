1d5fa966b822a7120de814b38afb252dbdcc563d
fractional-ray <isaiahherrera18@gmail.com>
2019-09-16 16:16:54 -0400

    adding naive_bayes

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,5 +1,7 @@
 """This module includes methods for training and predicting using naive Bayes."""
 import numpy as np
+from collections import defaultdict
+import pandas as pd 
 
 def prob(data, alpha, d, n):
     total = d * n 
@@ -11,6 +13,41 @@ def prob(data, alpha, d, n):
     #obabilites = (count[0] + alpha) / (total + 2 * alpha)
     return probabilites
 
+
+def find_classes(labels):
+    probabilites = defaultdict(list)
+    for y in range(len(labels)):
+        y_classes = labels[y]
+        if y_classes in probabilites:
+            probabilites[y_classes].append(y)
+        else:
+            probabilites[y_classes] = [y]
+
+    return probabilites
+
+def feature_probs(data,dict_labels):
+    fb = defaultdict(list)
+    for y in dict_labels:
+        if y not in fb:
+            fb[y] = []
+
+        feature_index = dict_labels[y]
+        tmp_data = np.ndarray(shape=(len(feature_index),5000))
+        print(len(dict_labels))
+        for index in range(len(feature_index)):
+            feature = data.T[:,index]
+            #feature = data[:,index]
+            #tmp_data[index] = feature 
+        #print(np.unique(tmp_data, return_counts = True))
+            counts = np.unique(feature, return_counts = True)
+            print(counts)
+            total = sum(counts[1])
+            true = counts[1][1]
+            prob = true/float(total)
+            print(prob)
+            fb[y].append(prob)
+
+    return fb 
 
 def conditional_prob(prob_A,prob_B):
     return (prob_A * prob_B)/(prob_A)
@@ -37,10 +74,14 @@ def naive_bayes_train(train_data, train_labels, params):
     d, n = train_data.shape
     num_classes = labels.size
     probs = prob(train_data, alpha, d, n)
-    for x in range(d):
-        y = (x + 1) % num_classes
-        cond_prob.append(conditional_prob(probs[x],probs[y]))
-        cond_prob_not.append(conditional_prob(1 - probs[x] , 1 - probs[y]))
+
+
+    dict_labels = find_classes(train_labels)
+    dict_probs = feature_probs(train_data, dict_labels)
+    #for x in range(d):
+    #    y = (x + 1) % num_classes
+    #    cond_prob.append(conditional_prob(probs[x],probs[y]))
+    #    cond_prob_not.append(conditional_prob(1 - probs[x] , 1 - probs[y]))
     #    cond_prob[1] = conditional_prob(probs[x],probs[index])
 
     # TODO: INSERT YOUR CODE HERE TO LEARN THE PARAMETERS FOR NAIVE BAYES
@@ -49,26 +90,35 @@ def naive_bayes_train(train_data, train_labels, params):
     model["conditions"] = cond_prob 
     model["priors"] = cond_prob_not 
 
-    return model
+    return dict_probs
 
 
 def bayes_prediction(sample, model):
-    prior = model["priors"]
-    conditions = model["conditions"]
+    #prior = model["priors"]
+    #conditions = model["conditions"]
     total = 1
     bayes_probility = []
-    for index in range(20):
-        word = sample[index]
-        if word == False:
-            value = np.log(prior[index])
-        else:
-            value = np.log(conditions[index])
+    for key in model:
+        class_prob = model[key]
+        #print(class_prob) 
+        #for feature in range(len(sample)):
+        for feature in range(len(class_prob) - 1):
+            if sample[feature] == False:
+                prob = 1 - class_prob[feature]
+            else:
+                prob = class_prob[feature]
+            total = total * prob
         
-        total += total *  value
-
-        bayes_probility.append(total)
+        bayes_probility.append( np.log(total))
+        #if word == False:
+        #    value = np.log(prior[index])
+        #else:
+        #    value = np.log(conditions[index])
         
+        #total += total *  value
 
+        #bayes_probility.append(total)
+        
     return np.argmax(bayes_probility)
 
 
@@ -88,6 +138,8 @@ def naive_bayes_predict(data, model):
     for index in range(len(data.T)):
         sample = data.T[index,:]
         prediction.append(bayes_prediction(sample,model))
+
+    print(prediction)
     return prediction
 
     # TODO: INSERT YOUR CODE HERE FOR USING THE LEARNED NAIVE BAYES PARAMETERS
